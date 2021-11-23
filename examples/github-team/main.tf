c283c5952fab98d62b1bff4d3002f1c74e296166
Soren Martius <soeren.martius@gmail.com>
2021-11-23 14:49:29 -0600

    chore: prepare v0.7.0 release

--- examples/github-team/main.tf
+++ examples/github-team/main.tf
@@ -35,7 +35,7 @@ resource "github_repository" "another_repository" {
 
 module "team" {
   source  = "mineiros-io/team/github"
-  version = "~> 0.6.0"
+  version = "~> 0.7.0"
 
   name        = "Engineering"
   description = "This team is created with terraform to test the terraformn-github-repository module."
@@ -55,7 +55,7 @@ module "team" {
 
 module "child_team" {
   source  = "mineiros-io/team/github"
-  version = "~> 0.6.0"
+  version = "~> 0.7.0"
 
   name           = "DevOps"
   parent_team_id = module.team.id
